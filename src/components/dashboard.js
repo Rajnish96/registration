9d2b3f5215eeaec6c9e9c5cf0ba7b1d97e17c5eb
Rajnish96 <rajnishkumarsingh93@gmail.com>
2023-05-15 00:43:06 +0530

    create router folder

---
+++ src/components/dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function dashboard() {
+    return (
+        <>
+            <h1>login Sucess</h1>
+            <button className='text-center'>logout</button>
+        </>
+    )
+}
